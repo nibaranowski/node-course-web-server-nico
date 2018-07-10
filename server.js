8fb03d2d51c0914b66602339295bcabd4ab77949
Nicolas Baranowski <nibaranowski@gmail.com>
2018-07-10 12:14:49 +0200

    fix server

--- server.js
+++ server.js
@@ -21,10 +21,7 @@ app.use((req, res, next) => {
 })
 
 app.use((req, res, next) => {
-    res.render('maintenance.hbs'), {
-        pageTitle: 'Site under maintenance',
-        welcomeMessage: 'Come back later'
-    });
+    res.render('maintenance.hbs');
 })
 
 app.use(express.static(__dirname + '/public'));
